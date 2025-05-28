50a307857261815e8c8134a461fb92be2d04e10b
elite-benni <benjamin.heiss@elite.tirol>
2025-05-28 23:30:15 +0200

    update angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HlmButtonDirective],
   template: ` <div
-    class="flex align-middle  items-center bg-primary w-full h-20"
-  >
-    <div class="w-full text-center text-xl ">Hello Spartans!</div>
-  </div>`,
+      class="flex align-middle  items-center bg-primary w-full h-20"
+    >
+      <div class="w-full text-center text-xl ">Hello Spartans!</div>
+    </div>
+    <div class="p-4">
+      <button hlmBtn variant="outline">Hello</button>
+    </div>`,
 })
 export class AppComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require("@spartan-ng/ui-core/hlm-tailwind-preset")],
+  presets: [require("@spartan-ng/brain/hlm-tailwind-preset")],
   content: ["./src/**/*.{html,ts}", "./libs/ui/**/*.{html,ts}"],
   theme: {
     extend: {},
