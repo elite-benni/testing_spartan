2e790ed605691f8b7680d252abfa43aba414f49c
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-06-02 10:04:22 +0200

    add tanstack table

---
+++ src/app/data-table/data-table.ts
@@ -0,0 +1,106 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  signal,
+} from '@angular/core';
+import {
+  HlmTableBodyDirective,
+  HlmTableCellDirective,
+  HlmTableDirective,
+  HlmTableFooterDirective,
+  HlmTableHeadDirective,
+  HlmTableHeaderDirective,
+  HlmTableRowDirective,
+  HlmTableVariantDefault,
+} from '../components/table.directives';
+import { columns, Payment } from './columns';
+import {
+  createAngularTable,
+  FlexRenderDirective,
+  getCoreRowModel,
+} from '@tanstack/angular-table';
+import { payments } from './payments';
+
+@Component({
+  selector: 'app-data-table-payment',
+  imports: [
+    HlmTableDirective,
+    HlmTableHeaderDirective,
+    HlmTableBodyDirective,
+    HlmTableFooterDirective,
+    HlmTableRowDirective,
+    HlmTableHeadDirective,
+    HlmTableCellDirective,
+
+    FlexRenderDirective,
+  ],
+  template: `
+    <div class="rounded-md border">
+      <table [hlm]="tableVariant()">
+        <thead>
+          @for( headerGroup of table.getHeaderGroups(); track headerGroup.id) {
+          <tr [attr.key]="headerGroup.id">
+            @for (header of headerGroup.headers; track header.id) { @if
+            (!header.isPlaceholder) {
+            <th>
+              <ng-container
+                *flexRender="
+                  header.column.columnDef.header;
+                  props: header.getContext();
+                  let header
+                "
+              >
+                <div [innerHTML]="header"></div>
+              </ng-container>
+            </th>
+            } }
+          </tr>
+          }
+        </thead>
+        <tbody>
+          @for (row of table.getRowModel().rows; track row.id) {
+          <tr
+            [attr.key]="row.id"
+            [attr.data-state]="row.getIsSelected() && 'selected'"
+          >
+            @for (cell of row.getVisibleCells(); track cell.id) {
+            <td>
+              <ng-container
+                *flexRender="
+                  cell.column.columnDef.cell;
+                  props: cell.getContext();
+                  let cell
+                "
+              >
+                <div [innerHTML]="cell"></div>
+              </ng-container>
+            </td>
+            }
+          </tr>
+          } @empty {
+          <tr>
+            <td class="text-center h-24" [attr.colspan]="columnsCount">
+              No results.
+            </td>
+          </tr>
+          }
+        </tbody>
+      </table>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DataTablePaymentComponent {
+  tableVariant = input(HlmTableVariantDefault);
+
+  payments = signal<Payment[]>(payments);
+
+  columnsCount = columns.length;
+
+  table = createAngularTable(() => ({
+    data: this.payments(),
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+  }));
+}
